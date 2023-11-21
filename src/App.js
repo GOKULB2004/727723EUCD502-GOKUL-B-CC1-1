3a2e64ff180823079bf52f1ae44ffebc3baf2d41
GOKULB2004 <151618856+GOKULB2004@users.noreply.github.com>
2023-11-21 16:58:56 +0530

    Add files via upload
    Question2

---
+++ src/App.js
@@ -0,0 +1,25 @@
+// ToggleText.jsx
+
+import React, { useState } from 'react';
+import './App.css'; // Adjust the path based on your project structure
+
+const ToggleText = () => {
+  const [isTextVisible, setIsTextVisible] = useState(false);
+
+  const handleToggleText = () => {
+    setIsTextVisible(!isTextVisible);
+  };
+
+  return (
+    <div className="toggle-text-container">
+      <button className="toggle-button" onClick={handleToggleText}>
+        {isTextVisible ? 'Hide' : 'Show'} 
+      </button>
+      <p className={`toggle-text ${isTextVisible ? 'visible' : ''}`}>
+        Some text to show or hide
+      </p>
+    </div>
+  );
+};
+
+export default ToggleText;
---
+++ src/App.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ToggleText from './App.js'; // Adjust the path based on your project structure
+
+const App = () => {
+  return (
+    <div>
+      <h1>Toggle Text App</h1>
+      <ToggleText />
+    </div>
+  );
+};
+
+export default App;
